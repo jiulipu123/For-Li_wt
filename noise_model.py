dc705a0c7c14bf22537f67288ac5c262a130ba4c
jiulipu123 <2459503149@qq.com>
2025-09-30 13:54:35 +0800

    Update noise_model.py

--- noise_model.py
+++ noise_model.py
@@ -3,6 +3,7 @@ import numpy as np
 from scipy import signal
 from scipy.integrate import trapezoid
 import matplotlib.pyplot as plt
+from scipy.constants import G,c,pi,au
 '''
 actual noise
 '''
@@ -82,11 +83,75 @@ def calculate_noise_psd(obs_data,instrument_noise,galactic_confusion=None):
     )
     return freqs,S_n
 
+#Lisa Noise 
+class LisaNoise:
+    def __init__(self,obs_data):
+        self.t=obs_data['obs_time']
+        self.N=len(self.t)
+        self.dt=obs_data['obs_dt']
+        self.T_obs_years=1
+        self.L_arm=2.5e9
+        self.f_star=c/(2*np.pi*self.L_arm)
+        self.freqs=np.fft.fftfreq(self.N,self.dt)
+
+        self.A_gal=1.14e-44
+        self.alpha_gal=1.8
+        self.f2_gal=0.31e-3
+        self.a1=-0.25
+        self.b1=-2.7
+        self.ak=-0.27
+        self.bk=-2.47
+        self.f1_gal=10**(self.a1*np.log10(self.T_obs_years)+self.b1)
+        self.fk_gal=10**(self.ak*np.log10(self.T_obs_years)+self.bk)
+
+    def calculate_OMS_PSD(self,f):
+        S_OMS_nu=(6.35e-12)**2*(1+(2e-3/f)**4)*(2*np.pi*f/c)**2
+        return S_OMS_nu
+    
+    def calculate_ACC_PSD(self,f):
+        term1=(2.4e-15)**2
+        term2=(1+(0.4e-3/f)**2)
+        term3=(1+(f/8e-3)**4)
+        S_acc_a=term1*term2*term3
+        S_acc_d=S_acc_a*(2*np.pi*f)**(-4)
+        S_acc_nu=S_acc_d*(2*np.pi*f/c)**2
+        return S_acc_nu
+    
+    def calculate_TDI_noise_PSD(self,f):
+        
+        omega=2*np.pi*f
+        L=self.L_arm
+        S_OMS=self.calculate_OMS_PSD(f=f)
+        S_acc=self.calculate_ACC_PSD(f=f)
+        
+        sin_term=np.sin(omega*L/c)
+        cos_term=np.cos(2*omega*L/c)
+        S_n=16*(sin_term)**2*(S_OMS+(3+cos_term)*S_acc)
+        return S_n
+    
+    def calculate_noise_strain(self,f):
+        S_OMS=self.calculate_OMS_PSD(f=f)
+        S_acc=self.calculate_ACC_PSD(f=f)
+        phiL=2*np.pi*f*self.L_arm/c
+        AvFXP2_approx=16*(3/20)*(1/(1+0.6*(2*np.pi*f*self.L_arm/c)**2))
+        S_hX=(S_OMS+S_acc*(3+np.cos(2*phiL)))/(phiL**2*AvFXP2_approx/4**2)
+        return S_hX
+
+
 obs_data=extract_obs_data(file_path)
 sky_signals=extract_all_sky_signals(file_path)
 instrument_noise_t=calculate_data_noise(obs_data,sky_signals)
 #galactic_confusion=calculate_galactic_confusion_noise(file_path)
-freqs,S_n=calculate_noise_psd(obs_data,instrument_noise_t)
+
+freqs,S_n_data=calculate_noise_psd(obs_data,instrument_noise_t)
+noise_model=LisaNoise(obs_data)
+
+# use postive frequency
+mask=(freqs>0)
+pos_freqs=freqs[mask]
+pos_S_n_data=S_n_data[mask]
+S_n=noise_model.calculate_TDI_noise_PSD(f=pos_freqs)
 plt.figure(figsize=(12,8))
-plt.loglog(freqs,S_n,'r')
+plt.loglog(pos_freqs,pos_S_n_data,'r')
+plt.loglog(pos_freqs,S_n,'b')
 plt.show()
